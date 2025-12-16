7b07407a640509f70ab04220878ccfcc00cb256e
joyndinda94 <ndindajoy94@gmail.com>
2025-12-16 11:34:33 +0300

    Create MethodQuestion1.java

---
+++ MethodQuestion1.java
@@ -0,0 +1,14 @@
+//joy ndinda j77-11541-2024
+//java program to calculate total cost
+public class VacationCostCalculator
+
+ //Method to calculate total cost
+ public static double calculateTotalCost(double accommodationCost, double mealCost, double activityCost) {
+        return accommodationCost + mealCost + activityCost;
+    }
+  public static void main(String[] args) {
+     double totalCost = calculateTotalCost(accommodation, meals, activities); 
+
+     System.out.println("Total Vacation Cost=" + totalCost);
+    }
+}
